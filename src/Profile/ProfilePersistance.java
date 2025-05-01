be341bd221a7058ef95fe3841d685f90019229f9
sadyanis <126914554+sadyanis@users.noreply.github.com>
2025-05-01 22:25:20 +0200

    ajout profile Persistance

--- src/Profile/ProfilePersistance.java
+++ src/Profile/ProfilePersistance.java
@@ -1,6 +1,11 @@
 package Profile;
+
+import java.util.List;
+
 // class responsable d'enregistrer le profile et le registery dans un fichier
 public interface ProfilePersistance {
 
     public void initProfileWithRegistery(Profile profile );
+    public void updateRegistery(List<FileData> files);
+
 }
